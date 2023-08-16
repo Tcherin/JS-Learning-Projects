3df079cdf1bdff98f35c9775db1163b7408c1366
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2023-08-16 22:30:13 +0100

    finished project 6

--- javascript-basic-projects/04-navbar/setup/app.js
+++ javascript-basic-projects/04-navbar/setup/app.js
@@ -7,3 +7,16 @@
 // remove - remove class
 
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  links.classList.toggle("show-links");
+});
